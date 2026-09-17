e8d61afb82a30a188675e8a4fee905f20c8826ee
sikbbang45822-create <sikbbang45822@gmail.com>
2026-09-18 05:17:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1362,31 +1362,10 @@ try:
     )
 
     # GeoJSON 지역명 → 프로젝트 데이터 지역명
-    region_name_map = {
-        "경상남도": "경남",
-        "경상북도": "경북",
-        "광주광역시": "광주",
-        "대구광역시": "대구",
-        "대전광역시": "대전",
-        "부산광역시": "부산",
-        "서울특별시": "서울",
-        "세종특별자치시": "세종",
-        "울산광역시": "울산",
-        "인천광역시": "인천",
-        "전라남도": "전남",
-        "전라북도": "전북",
-        "제주특별자치도": "제주",
-        "충청남도": "충남",
-        "충청북도": "충북",
-        "경기도": "경기",
-        "강원특별자치도": "강원특별자치도"
-    }
-    # 실제 GeoJSON 지역명 확인
-    geo_names = [
-        feature["properties"]["sidonm"]
+       region_name_map = {
+        feature["properties"]["sidonm"]: feature["properties"]["sidonm"]
         for feature in korea_geojson["features"]
-    ]
-
+    }
     st.write("GeoJSON 지역명:", geo_names)
 
     # Day 0 균형발전지수 연결
